17d0dccfb0f59add8ce2d85c560510c9b039a734
Yusuph <yusuphdarbo@gmail.com>
2025-12-11 11:13:05 +0000

    creating get res

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
 import client from './db/databasepg.js'
+import express from 'express'
 
-const res = await client.query('select * from post')
-console.log(res.rows)
+const app = express()
+const PORT = 8080
+
+app.length('/posts', async (reg, res) => {
+    try {
+        const result = await client.query("select * from post")
+        res.json(result.rows)
+    } catch (err) {
+        console.error(err)
+    }
+})
+
+app.listen(
+    PORT,
+    () => {console.log(`It is live on https://localhost:${PORT}`)}
+)
+
+// const res = await client.query('select * from post')
+// console.log(res.rows)
