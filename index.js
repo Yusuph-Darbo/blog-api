75dd575c9d0de853d9d19cc00027543b21c76a7b
Yusuph <yusuphdarbo@gmail.com>
2025-12-13 21:31:00 +0000

    Added get req by id

--- index.js
+++ index.js
@@ -9,11 +9,24 @@ app.get('/posts', async (reg, res) => {
         const result = await client.query("select * from post")
         res.json(result.rows)
     } catch (err) {
-        console.error(err)
+        res.status(500).json({ error: err.message });
+    }
+})
+
+// Getting post by id
+app.get('/posts/:id', async (reg, res) => {
+    try{
+        const result = await client.query("SELECT * FROM post WHERE post_id = $1")
+        if (result.rows.length === 0) {
+            return res.status(404).json({error: "Post not found"})
+        }
+        res.json(result.rows[0])
+    } catch {
+        res.status(500).json({ error: err.message });
     }
 })
 
 app.listen(
     PORT,
-    () => {console.log(`It is live on https://localhost:${PORT}`)}
+    () => {console.log(`It is live on http://localhost:${PORT}`)}
 )
