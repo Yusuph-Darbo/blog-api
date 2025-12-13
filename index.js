4afad7c0f3224cf855af465730539e598f99f8b5
Yusuph <yusuphdarbo@gmail.com>
2025-12-13 21:51:46 +0000

    added updating functionality

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.get('/posts', async (reg, res) => {
 // Getting post by id
 app.get('/posts/:id', async (req, res) => {
     // Get value not object
-    const {id}  = req.params
+    const id  = req.params.id
 
     try{
         const result = await client.query(
@@ -41,6 +41,34 @@ app.post('/posts', async (req, res) => {
             [title, content, category_id]
         )
         res.status(200).json(result.rows[0])
+    } catch (err) {
+        res.status(500).json({ error: err.message })
+    }
+})
+
+// Editing a post
+app.put('posts/:id', async (req,res) => {
+    const id  = req.params.id
+    const {title, content, category_id} = req.body
+
+    try{
+
+        const result = await client.query(
+            `UPDATE post
+             SET title = $1,
+                 content = $2,
+                 category_id = $3,
+                 updated_at = NOW()
+             WHERE post_id = $4
+             RETURNING *`,
+            [title, content, category_id, id]
+        )
+
+        if (result.rows.length === 0) {
+            return res.status(404).json({error: "Post not found"})
+        }
+
+        res.json(result.rows[0])
     } catch (err) {
         res.status(500).json({ error: err.message })
     }
