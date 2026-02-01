7a49c26ca1a1b05d0538e64494417ee2c33c1826
Yusuph Darbo <yusuphdarbo@gmail.com>
2026-02-01 15:28:28 +0000

    completed get req

--- src/index.ts
+++ src/index.ts
@@ -7,13 +7,47 @@ app.get("/posts", async (req, res) => {
   let query: string = "SELECT * FROM post";
 
   // Used to prevent SQL injection
-  const values: string[] = [];
+  /*
+    Values can be a string or an array of string
+    inside an array where each value can be a string or an array of strings
+    e.g. "tech", ["tech", "news"]
+    OR 
+    [
+        "tech",
+        ["react", "typescript"],
+        "backend"
+    ]
+   */
+  const values: (string | string[])[] = [];
   const conditions: string[] = [];
 
   // Checking if a query param was passed
   if (typeof category === "string") {
     values.push(category);
     conditions.push(`category = $${values.length}`);
+  }
+
+  if (typeof tags === "string") {
+    // tags can be comma-separated
+    const tagArray = tags.split(",");
+    values.push(tagArray);
+    // '&&' operator checks if arrays overlap
+    conditions.push(`tags && $${values.length}::text[]`);
+  }
+
+  if (conditions.length > 0) {
+    query += "WHERE " + conditions.join(" AND ");
+  }
+
+  try {
+    const result = await client.query(query, values);
+    res.status(200).json(result.rows);
+  } catch (err) {
+    if (err instanceof Error) {
+      res.status(500).json({ error: err.message });
+    } else {
+      res.status(500).json({ error: "Unknown error occurred" });
+    }
   }
 });
 
