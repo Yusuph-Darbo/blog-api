035d8726baba6e4ff8d84a243459f343044e7ab7
Yusuph Darbo <yusuphdarbo@gmail.com>
2026-02-01 15:14:53 +0000

    checking for query params

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,21 @@
 import app from "./server.js";
+import client from "./db/database.js";
+
+app.get("/posts", async (req, res) => {
+  const { category, tags } = req.query;
+
+  let query: string = "SELECT * FROM post";
+
+  // Used to prevent SQL injection
+  const values: string[] = [];
+  const conditions: string[] = [];
+
+  // Checking if a query param was passed
+  if (typeof category === "string") {
+    values.push(category);
+    conditions.push(`category = $${values.length}`);
+  }
+});
 
 app.listen(3000, () => {
   console.log("Server running at http://localhost:3000");
--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,6 @@ import express from "express";
 import type { Request, Response } from "express";
 
 const app = express();
-
-app.get("/", (req: Request, res: Response) => {
-  res.send("Server is running just fine.");
-});
+app.use(express.json());
 
 export default app;
