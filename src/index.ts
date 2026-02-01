47af9a907a3672d1de07d823d0e1ae9df3338236
Yusuph Darbo <yusuphdarbo@gmail.com>
2026-02-01 20:54:41 +0000

    endpoint to delete a post

--- src/index.ts
+++ src/index.ts
@@ -91,6 +91,10 @@ app.get("/posts/:id", async (req: Request, res: Response): Promise<void> => {
 app.put(
   "/posts/:id",
   async (
+    /* Req has a generic body that goes like this
+        Request<Params = any, ResBody = any, ReqBody = any, ReqQuery = any>
+        I specify the type of the params and the reqBody using my interface type
+    */
     req: Request<{ id: string }, any, postUpdated>,
     res: Response,
   ): Promise<void> => {
@@ -130,6 +134,35 @@ app.put(
     }
   },
 );
+
+// Deleting a post
+app.delete("/posts/:id", async (req: Request, res: Response): Promise<void> => {
+  const id = Number(req.params.id);
+
+  if (Number.isNaN(id)) {
+    res.status(400).json({ error: "Invalid post id" });
+    return;
+  }
+
+  try {
+    const result = await client.query(
+      "DELETE FROM post WHERE post_id = $1 RETURNING *",
+      [id],
+    );
+
+    if (result.rows.length === 0) {
+      res.status(404).json({ error: "Post not found" });
+    }
+
+    res.status(200).json({ mesage: `Post ${id} deleted successfully` });
+  } catch (err) {
+    if (err instanceof Error) {
+      res.status(500).json({ error: err.message });
+    } else {
+      res.status(500).json({ error: "Unknown error occurred" });
+    }
+  }
+});
 
 app.listen(3000, () => {
   console.log("Server running at http://localhost:3000");
